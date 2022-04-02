dcfbbf58387dbea03ccf8a6bbbcf1ecb71aa142d
Smr341 <smriti.misra9@gmail.com>
2022-04-02 23:07:17 +0530

    change

--- static/js/main.js
+++ static/js/main.js
@@ -26,29 +26,33 @@ $(document).ready(function () {
 
     // Predict
     $('#btn-predict').click(function () {
-        var form_data = new FormData($('#upload-file')[0]);
+        const months = ["January", "February", "March", "April", "May", "June", "July"];
 
-        // Show loading animation
-        $(this).hide();
-        $('.loader').show();
+        const random = Math.floor(Math.random() * months.length);
+        console.log(random, months[random]);
+        // var form_data = new FormData($('#upload-file')[0]);
 
-        // Make prediction by calling api /predict
-        $.ajax({
-            type: 'POST',
-            url: '/predict',
-            data: form_data,
-            contentType: false,
-            cache: false,
-            processData: false,
-            async: true,
-            success: function (data) {
-                // Get and display the result
-                $('.loader').hide();
-                $('#result').fadeIn(600);
-                $('#result').text(' Result:  ' + data);
-                console.log('Success!');
-            },
-        });
+        // // Show loading animation
+        // $(this).hide();
+        // $('.loader').show();
+
+        // // Make prediction by calling api /predict
+        // $.ajax({
+        //     type: 'POST',
+        //     url: '/predict',
+        //     data: form_data,
+        //     contentType: false,
+        //     cache: false,
+        //     processData: false,
+        //     async: true,
+        //     success: function (data) {
+        //         // Get and display the result
+        //         $('.loader').hide();
+        //         $('#result').fadeIn(600);
+        //         $('#result').text(' Result:  ' + data);
+        //         console.log('Success!');
+        //     },
+        // });
     });
 
 });
